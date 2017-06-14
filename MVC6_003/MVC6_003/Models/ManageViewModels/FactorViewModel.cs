22288e0ec0e211c34f010188a3713ca6ff5c5d67
T G <tomiksz@gmail.com>
2017-06-14 19:50:25 +0200

    add

---
+++ MVC6_003/MVC6_003/Models/ManageViewModels/FactorViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC6_003.Models.ManageViewModels
+{
+    public class FactorViewModel
+    {
+        public string Purpose { get; set; }
+    }
+}
